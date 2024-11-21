467800205087343cf0610db21dff069160d598e8
German G <german95guerrero@hotmail.com>
2024-11-21 15:16:55 -0500

    fix

--- dist/application/utils/validationID.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validationID = void 0;
-const validationID = (id) => {
-    if (isNaN(id) || !id || id < 0)
-        return { ok: false, message: "The entered ID must be a numeric value and is mandatory" };
-};
-exports.validationID = validationID;
-//# sourceMappingURL=validationID.js.map
